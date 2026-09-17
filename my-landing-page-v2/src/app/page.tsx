06eceb75be62e20908e2ff02fb8badb3b0885034
Marco-MDV <marcodevincentiis71@gmail.com>
2026-09-17 23:00:39 +0200

    add plugin for traffic

--- my-landing-page-v2/src/app/page.tsx
+++ my-landing-page-v2/src/app/page.tsx
@@ -18,6 +18,8 @@ console.log(
 import MyNavbar from "./components/navbar/myNavbar";
 import MyMain from "./components/main/myMain";
 import MdvLogo from "./components/text/mdvLogo";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
@@ -27,6 +29,8 @@ export default function Home() {
       </header>
       <MyMain />
       <footer className="text-[#71717a] text-sm flex flex-row flex-wrap justify-between items-center gap-10 p-5">
+        <Analytics/>
+        <SpeedInsights/>
         <MdvLogo myClassMdv="" />
         <p>© 2026 Marco De Vincentiis.</p>
         <button className="flex flex-row gap-2 justify-content-center items-center hover:text-[#a78bfa] duration-300 ">
